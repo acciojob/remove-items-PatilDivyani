cfb73df8d29edfd028c06b9b32ef2da0495e465f
PatilDivyani <125538181+PatilDivyani@users.noreply.github.com>
2023-05-19 16:21:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+function removeColor() {
+	let selectElement = document.getElementById("colorSelect");
+	let selectColor = selectElement.value;
+	
+	//Reomve the color
+	for (let i = 0; i < selectElement.length; i++) {
+		if(selectElement.option[i].value===selectColor){
+			selectElement.remove(i);
+			break;
+		}
+	}	
+}
+
+// get button to preform on clicking
+var removeButton = document.getElementById("removeButton");
+removeButton.addEventListener("click", removeColor);
