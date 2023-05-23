37a7306d71e4bd8c6eaca815ef346ea6a9192297
PatilDivyani <125538181+PatilDivyani@users.noreply.github.com>
2023-05-23 19:14:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 //your JS code here. If required.
+
+// get button to preform on clicking
+// let removeButton = document.getElementsByTagName("input");
+// removeButton.addEventListener("click", removeColor);
+
 function removeColor() {
-	let selectElement = document.getElementById("colorSelect");
-	let selectColor = selectElement.value;
-	
+	let selectElement = document.getElementsByTagName("select")[0];
+	let selectedColor = selectElement.value;
+	 
 	//Reomve the color
 	for (let i = 0; i < selectElement.length; i++) {
-		if(selectElement.option[i].value===selectColor){
-			selectElement.remove(i);
+		if(selectElement.children[i].value===selectedColor){
+            console.log("Item to be removed:" + selectElement.children[i].value)
+			// selectElement.children[i].remove();
 			break;
 		}
 	}	
 }
-
-// get button to preform on clicking
-var removeButton = document.getElementById("removeButton");
-removeButton.addEventListener("click", removeColor);
